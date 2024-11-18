af7e233fde890d96c20bda6d4062c49a64bfaa58
MonchoKarbo <146631954+MonchoKarbo@users.noreply.github.com>
2024-11-18 18:01:47 +0100

    Segunda subida

---
+++ src/main/java/com/mycompany/adicinanumero/AdicinaNumero.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adicinanumero;
+
+/**
+ *
+ * @author Moncho
+ */
+public class AdicinaNumero {
+    
+    public static void main(String[] args) {
+        new Ahorcado().jugar();
+        
+    }
+}
