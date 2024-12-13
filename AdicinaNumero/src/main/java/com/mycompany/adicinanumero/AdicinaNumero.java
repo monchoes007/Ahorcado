f870f159bc3d7c62cc4181b873a36379f5a02be2
Skirynmax <jose.alban.montoya@colexio-karbo.com>
2024-12-13 16:23:40 +0100

    mi oommit

---
+++ AdicinaNumero/src/main/java/com/mycompany/adicinanumero/AdicinaNumero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.adicinanumero;
+
+/**
+ *
+ * @author alban
+ */
+public class AdicinaNumero {
+
+   public static void main(String[] args) {
+        new Ahorcado().jugar();
+    }   
+}
